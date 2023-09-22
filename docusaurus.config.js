8484ec7ee6aa274893fd092155192eb74fdd1514
JasonMAnderson <jasonzencove@gmail.com>
2023-09-22 13:38:20 -0700

    set baseURL to / and URL to pxl site

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,11 +11,12 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://planetx-labs.github.io',
+  // url: 'https://planetx-labs.github.io',
+  url: 'https://docs.planetxlabs.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Documentation/',
-  // baseUrl: '/',
+  // baseUrl: '/Documentation/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
