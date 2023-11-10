d35134a97dd6a5c00db5d62c2f6202b5c04002ff
JasonMAnderson <jasonzencove@gmail.com>
2023-11-10 09:58:10 -0800

    updated images

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'Take Control of Your Data',
-    Svg: require('@site/static/img/datainsomanyplaces_2.svg').default,
+    Svg: require('@site/static/img/control-your-data.svg').default,
     description: (
       <>
         Dasset frees you from the dilemma of having your data spread across multiple devices
@@ -27,7 +27,7 @@ const FeatureList = [
   },
   {
     title: 'Access from Anywhere',
-    Svg: require('@site/static/img/dataanywhere_3.svg').default,
+    Svg: require('@site/static/img/access-data-anywhere.svg').default,
     description: (
       <>
         Whether you are next to your Dasset server, or half-way across the world, getting access to
